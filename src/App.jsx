44d3c1ad4cbed0479585e445fc22580796f155ac
Jefferson Alves <jefff9910@outlook.com>
2023-10-15 19:07:49 -0300

    inicio de sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Header } from './components/Header'
+import style from './App.module.css'
 import './global.css'
+import { Post } from './Post'
+import { Sidebar } from './components/Sidebar'
 
 
 function App() {
@@ -7,7 +10,13 @@ function App() {
     <>
      <Header></Header> 
 
-     <h1>Ola Mundo</h1>
+     <div className={style.wrapper}>
+        <Sidebar></Sidebar>
+        <main>
+          <Post></Post>
+          <Post></Post>
+        </main>
+     </div>
     </>
   )
 }
