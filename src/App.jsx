48b5bc6103660be617c680a1bdc8077532c6279c
Jefferson Alves <jefff9910@outlook.com>
2023-10-15 17:47:33 -0300

    Iniciando aplicacao

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from './components/Header'
+import './global.css'
+
+
+function App() {
+  return (
+    <>
+     <Header></Header> 
+
+     <h1>Ola Mundo</h1>
+    </>
+  )
+}
+
+export default App
