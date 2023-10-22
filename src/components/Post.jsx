d2bdad34a0cc16f00a471df89b8252f3c98d46be
Jefferson Alves <jefff9910@outlook.com>
2023-10-22 13:22:12 -0300

    Post created

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -15,17 +15,29 @@ export function Post() {
 
                 <time title='01 de janeiro 2022' dateTime="2022-01-01 13:00:00">Publicado há 1h</time>
             </header>
-            <div>
-            <p>Fala galeraa 👋</p>
-            <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-            <p>👉 <a href="">jane.design/doctorcare</a></p>
-            <p>
-                <a href="">#novoprojeto </a>
-                <a href="">#nlw </a>
-                <a href="">#rocketseat</a>
-            </p>
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                    <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                    <p><a href="">jane.design/doctorcare</a></p>
+                    <p>
+                        <a href="">#novoprojeto </a>{' '}
+                        <a href="">#nlw </a> {' '}
+                        <a href="">#rocketseat</a>
+                    </p>
             </div>
 
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea 
+                    placeholder='Deixe seu comentário'
+                />
+
+                <footer>
+                    <button type='submit'>Publicar</button>
+                </footer>
+                
+            </form>
 
         </article>
     </>
